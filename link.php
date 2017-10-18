d927ea3d5f4002619471ce2f65842a6dbdd24b08
Misty Lackie <mistylackie@users.noreply.github.com>
2017-10-17 19:14:01 -0700

    Update link.php

--- link.php
+++ link.php
@@ -8,10 +8,12 @@ $tid = n($_GET['tid']); //email template id
 $url = e($_GET['url']); //url user clicked
 $type = e($_GET['type']); //link or open
 
-if ($type === "link" && !empty($url)) {
-    $recordTracking->addLinkClick($type, $uid, $tid, $url);
-    header("Location: " . $url);
-} else {
-    $recordTracking->addOpen($type, $uid, $tid);
-    header("Location: ".SCRIPT_URL."assets/images/transimage.gif");
+if (!empty($type) {
+    if ($type === "link" && !empty($url)) {
+        $recordTracking->addLinkClick($type, $uid, $tid, $url);
+        header("Location: " . $url);
+    } else {
+        $recordTracking->addOpen($type, $uid, $tid);
+        header("Location: ".SCRIPT_URL."assets/images/transimage.gif");
+    }
 }
